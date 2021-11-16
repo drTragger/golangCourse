0ac573bcdf55d7b2b2de57bd3eafe193761d12d1
misha <mishaponomarenko11082001@gmail.com>
2021-11-16 15:14:33 +0200

    Getting first and last characters from string

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	word := "Привет Вася"
+	firstChar := string([]rune(word)[0])
+	lastChar := string([]rune(word)[utf8.RuneCountInString(word)-1:])
+	fmt.Printf("First char: %s\nLast char: %s\n", firstChar, lastChar)
 }
